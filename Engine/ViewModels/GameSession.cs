19da0cbc0657d52032b29f2f998643bc5aac865b
Alex <alejandro@bocbanking.com>
2022-04-20 23:29:40 -0500

    Added CurrentPlayer to UI

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace Engine.ViewModels
 {
     public class GameSession
     {
+        public Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player()
+            {
+                Name = "Alex",
+                CharacterClass = "Test",
+                HitPoints = 12,
+                ExpPoints = 0,
+                Level = 1,
+                Gold = 1000000
+            };
+        }
     }
 }
